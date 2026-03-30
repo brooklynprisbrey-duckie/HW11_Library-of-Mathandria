3817728cc567ea8c531487bcd5df23039650c90d
brooklynprisbrey-duckie <brooklynprisbrey@mail.weber.edu>
2026-03-30 01:48:01 -0600

    Linker 2019 error... scrubbing through to find issue

---
+++ inc/bmath.h
@@ -0,0 +1,25 @@
+#include <iomanip>
+#include <iostream>
+#pragma once
+
+template <typename generiX>
+generiX absolute(generiX X);
+
+template <typename generiX>
+generiX power(generiX X, int n);
+
+long long factorial(int n);
+
+template <typename generiX>
+double squareRoot(generiX X);
+
+double pi();
+
+template <typename generiX>
+double sine(generiX X);
+
+template <typename generiX>
+double cosine(generiX X);
+
+template <typename generiX>
+double tangent(generiX X);
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,42 @@
-#include <iostream>
+#include "bmath.h"
+
 using namespace std;
 
 int main(){
-    cout << "hello world" << endl;
+    cout << setprecision(20) << endl;
+
+    //pi() uses power(x,n)
+    cout << "pi(): " << pi() << endl << endl;
+
+    // squareRoot(x) uses absolute(x)
+    cout << "squareRoot(2): " << squareRoot(2) << endl;
+    cout << "squareRoot(6.75f): " << squareRoot(6.75f) << endl;
+    cout << "squareRoot(15.99): " << squareRoot(15.99) << endl;
+    cout << "squareRoot('a'): " << squareRoot('a') << endl;
+    cout << "squareRoot(0): " << squareRoot(0) << endl;
+    cout << "squareRoot(-1): " << squareRoot(-1) << endl << endl;
+
+    //sine(x) uses factorial(n)
+    cout << "sine(1): " << sine(1) << endl;
+    cout << "sine(5.25f): " << sine(5.25f) << endl;
+    cout << "sine(pi()): " << sine(pi()) << endl;
+    cout << "sine(-5*pi()/2): " << sine(-5*pi()/2) << endl;
+    cout << "sine('a'): " << sine('a') << endl;
+    cout << "sine(10): " << sine(10) << endl << endl;
+
+    cout << "cosine(1): " << cosine(1) << endl;
+    cout << "cosine(5.25f): " << cosine(5.25f) << endl;
+    cout << "cosine(pi()): " << cosine(pi()) << endl;
+    cout << "cosine(-5*pi()/2): " << cosine(-5*pi()/2) << endl;
+    cout << "cosine('a'): " << cosine('a') << endl;
+    cout << "cosine(10): " << cosine(10) << endl << endl;
+
+    cout << "tangent(1): " << tangent(1) << endl;
+    cout << "tangent(5.25f): " << tangent(5.25f) << endl;
+    cout << "tangent(pi()): " << tangent(pi()) << endl;
+    cout << "tangent(-5*pi()/2): " << tangent((-5*pi())/2) << endl;
+    cout << "tangent('a'): " << tangent('a') << endl;
+    cout << "tangent(10): " << tangent(10) << endl;
+
+    return 0;
 }
